7a662fc1811827384f9f7fbcb19b4a03b60a4bde
JEdoFernandez <juanedozgz@hotmail.com>
2023-09-10 23:07:20 +0200

    Main creado para encontrar el número de lavadoras, pero no encuentro ningún elemento con esos nombres en la página

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        // Iniciar el WebDriver
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        // Añadir URL de la página
+        driver.get("https://www.elcorteingles.es/electrodomesticos/lavadoras/");
+
+        // Encontrar los productos de la pagina
+        WebElement productList = driver.findElement(By.className("js_preview_image")); // Adjust the selector as needed
+
+        // Contar el número de lavadoras en la página
+        int actualProductCount = productList.findElements(By.className("js_preview_image")).size();
+
+        System.out.println(actualProductCount);
+
+        // Coincidir con el número de productos que hemos contado en la página
+        int expectedProductCount = 25; // Replace with the expected count
+        if (actualProductCount == expectedProductCount) {
+            System.out.println("Test correcto: Mismo número de Lavadoras.");
+        } else {
+            System.out.println("Test Incorrecto: distinto número de Lavadoras.");
+        }
+
+        // Cerrar el Webdriver
+        driver.quit();
     }
 }
